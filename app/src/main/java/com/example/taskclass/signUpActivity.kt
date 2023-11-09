669184ec519822ee4f3175ce07afc7c3080a5313
syedhamza-alt <syedhamzaimran3@gmail.com>
2023-11-09 21:54:24 +0500

    commit 11/09/23

--- app/src/main/java/com/example/taskclass/signUpActivity.kt
+++ app/src/main/java/com/example/taskclass/signUpActivity.kt
@@ -1,12 +1,17 @@
 package com.example.taskclass
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.EditText
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.taskclass.databinding.ActivitySignUpBinding
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.DoubleBounce
+
 
 class signUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -17,37 +22,39 @@ class signUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.SignUpBtn.setOnClickListener {
-            val name=binding.nameEt.editText?.text?.toString();
-            val pass=binding.passEt.editText?.text?.toString();
-            val email=binding.emailEt.editText?.text?.toString();
-            val phoneNum=binding.numberEt.editText?.text?.toString();
+            val name = binding.nameEt.editText?.text?.toString();
+            val pass = binding.passEt.editText?.text?.toString();
+            val email = binding.emailEt.editText?.text?.toString();
+            val phoneNum = binding.numberEt.editText?.text?.toString();
 
-                if (TextUtils.isEmpty(binding.nameEt.editText?.text.toString().trim())) {
-                    binding.nameEt.setError("Name cannot be empty")
-                }
-                if (TextUtils.isEmpty(binding.emailEt.editText?.text.toString().trim())) {
-                    binding.emailEt.setError("Email cannot be empty")
-                }
-                if (TextUtils.isEmpty(binding.passEt.editText?.text.toString().trim())) {
-                    binding.passEt.setError("Password cannot be empty")
-                }
-                if (TextUtils.isEmpty(binding.numberEt.editText?.text.toString().trim())){
-                    binding.numberEt.setError("Phone number can not be empty")
-            }else{
-                Toast.makeText(applicationContext,"SignUp successful",Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(binding.nameEt.editText?.text.toString().trim())) {
+                binding.nameEt.setError("Name cannot be empty")
+            }
+            if (TextUtils.isEmpty(binding.emailEt.editText?.text.toString().trim())) {
+                binding.emailEt.setError("Email cannot be empty")
+            }
+            if (TextUtils.isEmpty(binding.passEt.editText?.text.toString().trim())) {
+                binding.passEt.setError("Password cannot be empty")
+            }
+            if (TextUtils.isEmpty(binding.numberEt.editText?.text.toString().trim())) {
+                binding.numberEt.setError("Phone number can not be empty")
+            } else {
+                val progressBar = findViewById<View>(R.id.progress) as ProgressBar
+                val doubleBounce: Sprite = DoubleBounce()
+                Toast.makeText(applicationContext, "SignUp successful", Toast.LENGTH_SHORT).show();
 
-                val i=Intent(applicationContext,loginActivity::class.java);
+                val i = Intent(applicationContext, loginActivity::class.java);
                 startActivity(i);
+                progressBar.indeterminateDrawable = doubleBounce
+
             }
 
 
         }
-            binding.signUpTv.setOnClickListener{
-                val i=Intent(applicationContext,loginActivity::class.java);
-                startActivity(i);
-            }
-
-
+        binding.signUpTv.setOnClickListener {
+            val i = Intent(applicationContext, loginActivity::class.java);
+            startActivity(i);
+        }
 
 
     }
